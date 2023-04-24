5415dbfb97c0bb7eb5c625d53bdc9f4b345fb94d
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-04-24 14:48:46 -0600

    :sparkles:Implementacion de cliente http DIO

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
+  static String baseUrl = dotenv.env['BASE_URL'] ?? '';
   static String theMovieDbKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'No existe';
 }
---
+++ lib/infraestructure/moviesdb_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+
+class MoviesdbDatasource extends MoviesDataSource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.baseUrl,
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'lenguage': 'es-MX'
+      },
+    ),
+  );
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/now_playing');
+    final List<Movie> movies = [];
+    return movies;
+  }
+}
