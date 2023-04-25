4d3c4a6feab7b0d973f8823a7c9c9326e6187bd7
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-04-25 14:59:10 -0600

    :sparkles:Implementacion del proveedor de estado

---
+++ lib/presentation/providers/movies/movies_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+
+// Definicion del proveedor del estado
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>(
+  (ref) {
+    final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying();
+    return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+  },
+);
+
+// Definicion de la referencia a la funcion que va a recibir la clase que controla al proveedor del estado
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+// Clase que controla al proveedor del estado
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  late MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({required fetchMoreMovies}) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);
+    state = [...state, ...movies];
+  }
+}
