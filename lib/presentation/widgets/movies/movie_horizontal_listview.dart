aa7aa22e73d45224346521e79f418cc73626f864
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-05-08 16:09:26 -0600

    :sparkles: Add helper HumanFormats (Package intl)

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:flutter/material.dart';
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -121,13 +122,21 @@ class _Slide extends StatelessWidget {
             width: 150,
             child: Row(
               children: [
+                //Icon Start
                 Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
                 const SizedBox(width: 3),
+
+                // Puntaje
                 Text('${movie.voteAverage}',
                     style: textStyles.bodyMedium
                         ?.copyWith(color: Colors.yellow.shade800)),
                 const Spacer(),
-                Text('${movie.popularity}', style: textStyles.bodySmall),
+
+                // Popularidad
+                Text(
+                  HumanFormats.number(movie.popularity),
+                  style: textStyles.bodySmall,
+                ),
               ],
             ),
           )
